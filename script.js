6f43e2c7960e955b608e36ca23350229ad0862a9
Sai <vsaishankar25@gmail.com>
2021-07-03 23:41:23 +0530

    sorting added

--- script.js
+++ script.js
@@ -3,38 +3,16 @@ var requestOptions = {
   redirect: "follow",
 };
 
+fetch("https://disease.sh/v3/covid-19/countries?sort=active", requestOptions)
+  .then((response) => response.json())
+  .then((result) => appendData(result))
+  .catch((error) => console.log("error", error));
 
+var table = document.getElementById("table");
 
-    fetch("https://disease.sh/v3/covid-19/countries?sort=active", requestOptions)
-        .then((response) => response.json())
-        .then((result) => appendData(result))
-        .catch((error) => console.log("error", error));
-
-var table = document.getElementById("table")
-
-function sortTable(type) {
-    if (type == 1) {
-        fetch(
-          "https://disease.sh/v3/covid-19/countries?sort=active",
-          requestOptions
-        )
-          .then((response) => response.json())
-          .then((result) => appendData(result))
-          .catch((error) => console.log("error", error));
-    }
-    if (type == 2) {
-        fetch(
-          "https://disease.sh/v3/covid-19/countries?sort=recovered",
-          requestOptions
-        )
-          .then((response) => response.json())
-          .then((result) => appendData(result))
-          .catch((error) => console.log("error", error));
-    }
-}
 function appendData(data) {
   var num = parseInt(1);
-
+    table.innerHTML = ""
   for (var i = 0; i < data.length; i++) {
     table.innerHTML +=
       "<tr>" +
@@ -59,4 +37,44 @@ function appendData(data) {
       "</tr>";
     num++;
   }
+}
+
+function sort(type) {
+    if (type == 1) {
+      fetch(
+        "https://disease.sh/v3/covid-19/countries?sort=active",
+        requestOptions
+      )
+        .then((response1) => response1.json())
+        .then((result1) => appendData(result1))
+            .catch((error) => console.log("error", error));
+        
+    }
+    if (type == 2) {
+        fetch(
+            "https://disease.sh/v3/covid-19/countries?sort=recovered",
+            requestOptions
+        )
+            .then((response1) => response1.json())
+            .then((result1) => appendData(result1))
+            .catch((error) => console.log("error", error));
+    }
+    if (type == 3) {
+      fetch(
+        "https://disease.sh/v3/covid-19/countries?sort=deaths",
+        requestOptions
+      )
+        .then((response1) => response1.json())
+        .then((result1) => appendData(result1))
+        .catch((error) => console.log("error", error));
+    }
+    if (type == 4) {
+      fetch(
+        "https://disease.sh/v3/covid-19/countries?sort=cases",
+        requestOptions
+      )
+        .then((response1) => response1.json())
+        .then((result1) => appendData(result1))
+        .catch((error) => console.log("error", error));
+    }
 }
